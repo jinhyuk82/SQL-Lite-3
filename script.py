dc536b3882923c74b733b074534e763a149fa352
jinhyuk <jinhyuk.seo@gmail.com>
2017-10-13 22:58:36 +0400

    Create table and Insert the values and View.

--- script.py
+++ script.py
@@ -1,7 +1,30 @@
 import sqlite3
 
-conn=sqlite3.connect("lite.db")
-cur=conn.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
-conn.commit()
-conn.close()
+def create_table():
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+    conn.commit()
+    conn.close()
+
+def insert(item,quantity,price):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO store VALUES(?,?,?)", (item,quantity,price))
+    conn.commit()
+    conn.close()
+
+insert("Water Glass", 3, 6.5)
+insert("Wine Glass", 5, 2.4)
+insert("Coffee Glass", 30, 9.5)
+insert("Juice Glass", 100, 3.1)
+
+def view():
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM store")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+print(view())
