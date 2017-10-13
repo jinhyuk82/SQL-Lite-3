87cd0d5c649e078719c3d92457bf8c92726475c6
jinhyuk <jinhyuk.seo@gmail.com>
2017-10-13 23:17:01 +0400

    Delete and Update method

--- script.py
+++ script.py
@@ -7,17 +7,17 @@ def create_table():
     conn.commit()
     conn.close()
 
-def insert(item,quantity,price):
+def insert(item, quantity, price):
     conn=sqlite3.connect("lite.db")
     cur=conn.cursor()
-    cur.execute("INSERT INTO store VALUES(?,?,?)", (item,quantity,price))
+    cur.execute("INSERT INTO store VALUES(?,?,?)", (item, quantity, price))
     conn.commit()
     conn.close()
 
-insert("Water Glass", 3, 6.5)
-insert("Wine Glass", 5, 2.4)
-insert("Coffee Glass", 30, 9.5)
-insert("Juice Glass", 100, 3.1)
+#insert("Water Glass", 3, 6.5)
+#insert("Wine Glass", 5, 2.4)
+#insert("Coffee Glass", 30, 9.5)
+#insert("Juice Glass", 100, 3.1)
 
 def view():
     conn=sqlite3.connect("lite.db")
@@ -26,5 +26,22 @@ def view():
     rows=cur.fetchall()
     conn.close()
     return rows
+
+def delete(item):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM store WHERE item=?", (item,))
+    conn.commit()
+    conn.close()
+
+def update(quantity, price, item):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?", (quantity, price, item))
+    conn.commit()
+    conn.close()
+
+#delete("Water Glass")
+update(4, 2.8, "Coffee Glass")
 
 print(view())
